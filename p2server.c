7c3ac87475c8375c0a03d60358a741785a167f08
jshulbs <jshulbs@gmail.com>
2017-03-05 15:43:17 -0500

    Update p2server.c

--- p2server.c
+++ p2server.c
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
             exit(-1);
         }
         else {
-            printf("Client: " %s, buffer);
+            printf("Client: " %s "\n", buffer);
         }
         close(aconn);                               //close connection
     }    
